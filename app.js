ed2c7083ecec6f2c14bf4c8e4ce5b45c3ea2b7d7
wildkard <jimdiwatin@gmail.com>
2016-10-06 13:14:33 +0800

    Update app.js

--- app.js
+++ app.js
@@ -4,13 +4,13 @@
 
 var config = {
   // - Your app's id on moneypot.com
-  app_id: 18,                             // <----------------------------- EDIT ME!
+  app_id: 2331,                             // <----------------------------- EDIT ME!
   // - Displayed in the navbar
   app_name: 'Untitled Dice',
   // - For your faucet to work, you must register your site at Recaptcha
   // - https://www.google.com/recaptcha/intro/index.html
-  recaptcha_sitekey: '6LfI_QUTAAAAACrjjuzmLw0Cjx9uABxb8uguLbph',  // <----- EDIT ME!
-  redirect_uri: 'https://untitled-dice.github.io',
+  recaptcha_sitekey: '6Ld-jwgUAAAAAEM4s-GmFmcreAMpV3Pb4k3Xs-2r',  // <----- EDIT ME!
+  redirect_uri: 'https://wildkard.github.io/',
   mp_browser_uri: 'https://www.moneypot.com',
   mp_api_uri: 'https://api.moneypot.com',
   chat_uri: '//socket.moneypot.com',
